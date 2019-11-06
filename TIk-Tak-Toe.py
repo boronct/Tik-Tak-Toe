4b69a07497811d263c20eb5bfa0b38f9441e46a0
richardhwhite3 <46230694+richardhwhite3@users.noreply.github.com>
2019-11-05 20:58:27 -0500

    Initial commit

---
+++ TIk-Tak-Toe.py
@@ -0,0 +1,16 @@
+from graphics import *
+
+def main():
+    win = GraphWin("Tik-Tak-Toe", 600, 600)
+
+    l1 = Line(Point(0,200),Point(600,200)).draw(win)
+    l1.setWidth(10)
+    l2 = Line(Point(0,400),Point(600,400)).draw(win)
+    l2.setWidth(10)
+    l3 = Line(Point(200,0),Point(200,600)).draw(win)
+    l3.setWidth(10)
+    l4 = Line(Point(400,0),Point(400,600)).draw(win)
+    l4.setWidth(10)
+
+    win.getMouse()
+main()
